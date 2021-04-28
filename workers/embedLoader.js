665aca5bb374f9229950d190ad16ec1a190a993f
Anurag <anuraglama5@gmail.com>
2021-04-28 11:04:45 +0530

    Update embedLoader.js

--- workers/embedLoader.js
+++ workers/embedLoader.js
@@ -1,8 +1,8 @@
-function begin (e){
+function embedLoad(){
   console.log('Message received. Starting Work');
   document.getElementById("embed1").innerHTML="<iframe loading='lazy' width='560' height='315' src='https://www.youtube.com/embed/cWPsLvrF3MM' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
   document.getElementById("embed2").innerHTML="<iframe loading='lazy' width='560' height='315' src='https://www.youtube.com/embed/iwvOREzipjE' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
   
 
 }
-begin();
+embedLoad();
